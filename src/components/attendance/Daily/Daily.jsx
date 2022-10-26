4e6f9a0680cfc6edadad62e23a88f5231e5534f9
Ban Cortez <101543597+haynakoban@users.noreply.github.com>
2022-10-27 05:21:51 +0800

    Update Daily.jsx

--- src/components/attendance/Daily/Daily.jsx
+++ src/components/attendance/Daily/Daily.jsx
@@ -29,15 +29,15 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 import TablePaginationActions from '../TablePaginationActions';
 import DailyModal from './DailyModal';
-import { AuthContext } from '../../lib/authContext';
+import { AuthContext } from '../../../lib/authContext';
 import {
   DailyAttendanceDateFormatter,
   TimeFormatter,
-} from '../../lib/DateFormatter';
+} from '../../../lib/DateFormatter';
 import {
   fetchDailyAttendance,
   getDailyAttendances,
-} from '../../features/attendances/attendancesSlice';
+} from '../../../features/attendances/attendancesSlice';
 
 const Daily = ({ company_id }) => {
   const { _user } = useContext(AuthContext);
